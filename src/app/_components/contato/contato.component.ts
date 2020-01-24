38f46f2232da1d40f92cf17332b1a7d53b2343ad
Gabriel Maia <gabrielmaiasbu@gmail.com>
2020-01-24 17:28:21 -0300

    Contatos ok

--- src/app/_components/contato/contato.component.ts
+++ src/app/_components/contato/contato.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeOutRight } from 'ng-animate';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
-  styleUrls: ['./contato.component.scss']
+  styleUrls: ['./contato.component.scss'],
+  animations: [
+    trigger('fadeOutRight', [transition('* => *', useAnimation(fadeOutRight, {
+      params: { timing: 4, delay: 1 }
+    }))])
+  ],
 })
 export class ContatoComponent implements OnInit {
 
