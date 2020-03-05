017d1b99ed80dcbd8177e0ebd76cafab054e2fe3
f5promotora <desenvolvedor8@f5promotora.com.br>
2020-03-05 16:58:55 -0300

    Produtos refeitos

--- src/app/_components/contato/contato.component.ts
+++ src/app/_components/contato/contato.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { bounceInUp } from 'ng-animate';
 import { trigger, transition, useAnimation } from '@angular/animations';
-import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
 
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
-  providers: [NgbRatingConfig],
+  providers: [],
   styleUrls: ['./contato.component.scss'],
   animations: [
     trigger('bounceInUp', [transition('* => *', useAnimation(bounceInUp, {
@@ -21,9 +20,8 @@ export class ContatoComponent implements OnInit {
   qtdEstrelas = 3;
   bounceInUp: any;
 
-  constructor(config: NgbRatingConfig) {
-    config.max = 5;
-    config.resettable = true;
+  constructor() {
+    
    }
 
   ngOnInit() {
--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { bounceInUp } from 'ng-animate';
+import { trigger, transition, useAnimation } from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('bounceInUp', [transition('* => *', useAnimation(bounceInUp, {
+      params: { timing: 4 }
+    }))])
+  ],
 })
 export class HomeComponent implements OnInit {
-  images = [100, 400, 400].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  bounceInUp: any;
   constructor() { }
 
   ngOnInit() {
